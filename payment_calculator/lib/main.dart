ff0f24b44eb7a2de326dca0ea11f2b55603f0940
seamoooo <hm.seamo@gmail.com>
2022-12-29 03:31:27 +0900

    add from

--- payment_calculator/lib/main.dart
+++ payment_calculator/lib/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'calc App',
+      theme: ThemeData(primarySwatch: Colors.purple),
       home: MyHomePage(),
     );
   }
@@ -16,21 +17,42 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter App'),
-        ),
-        body: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                width: double.infinity,
-                child: Card(
-                  color: Colors.blue,
-                  child: Text('Flutter'),
-                  elevation: 5,
+      appBar: AppBar(
+        title: Text('助家 お会計計算機'),
+      ),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'お名前',
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: '入店時間',
+              ),
+            ),
+            ElevatedButton(
+              child: Text(
+                '新規登録',
+                style: TextStyle(
+                  color: Colors.purple,
+                  fontSize: 12,
                 ),
               ),
-            ]));
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white, //ボタンの背景色
+              ),
+              onPressed: () => {},
+            )
+          ]),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => "",
+      ),
+    );
   }
 }
